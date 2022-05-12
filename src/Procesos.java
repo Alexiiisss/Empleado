c9b5d58db072f5d884c4a9a099b72842f1f902da
Sebastian Alexis Lopez Cespedes <sebatian.cespedes@correounivalle.edu.co>
2022-05-12 14:23:43 -0500

    Updated

---
+++ src/Procesos.java
@@ -0,0 +1,23 @@
+
+import java.util.ArrayList;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author sebas
+ */
+public class Procesos {
+    
+    private ArrayList<String> Empleado; 
+    
+    public Procesos()
+            {
+                Empleado = new ArrayList<>();
+            }
+    
+    
+}
